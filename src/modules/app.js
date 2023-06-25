cc6d8947458e1b8701a9bb96c983384a020caaa1
Ayosef <syrivera@uc.cl>
2023-06-25 15:02:39 -0400

    Add the todo factory to the app module

---
+++ src/modules/app.js
@@ -0,0 +1,12 @@
+const todoFactory = (title, description, duedate, priority, notes, checklist) => {
+    const getTitle = () => title;
+    const getDescription = () => description;
+    const getdueDate = () => duedate;
+    const getPriority = () => priority;
+    const getNotes = () => notes;
+    const getChecklist = () => checklist;
+
+    return {getTitle, getDescription, getdueDate, getPriority, getNotes, getChecklist};
+};
+
+export default todoFactory;
